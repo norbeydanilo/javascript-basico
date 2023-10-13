ba3abf1580f62811447d4e5b2cd54a269b1685ea
Norbey Danilo <24207969+ndmc98@users.noreply.github.com>
2023-10-13 16:02:26 -0500

    update

--- arrays/ejercicios-03.js
+++ arrays/ejercicios-03.js
@@ -46,5 +46,42 @@ const _gatito = {
 let { amigos } = _gatito;
 console.log(amigos)
 
+// ******************************
+
+const mascota1 = {
+    _nombre: 'Max',
+    _edad: 3,
+    caracteristicas: {
+      _tamanio: 'Mediano',
+      _color: 'Marrón',
+      raza: 'Labrador'
+    },
+    propietario: {
+      nombre: 'Juan',
+      direccion: {
+        calle: 'Av. Principal',
+        ciudad: 'Madrid',
+        codigoPostal: '28001'
+      }
+    }
+  };
+
+const {
+    _nombre,
+    _edad,
+    caracteristicas: { _tamanio, _color, raza },
+    propietario: { nombre: nombrePropietario, direccion: { calle, ciudad, codigoPostal } }
+  } = mascota1;
+  
+  console.log(nombre);           // Max
+  console.log(edad);             // 3
+  console.log(_tamanio);          // Mediano
+  console.log(_color);            // Marrón
+  console.log(raza);             // Labrador
+  console.log(nombrePropietario); // Juan
+  console.log(calle);            // Av. Principal
+  console.log(ciudad);           // Madrid
+  console.log(codigoPostal);      // 28001
+
 // En los arrays no es necesario tener el mismo nombre pero sí el orden
 // En los objetos si es necesario tener el mismo nombre para la desestructuración
